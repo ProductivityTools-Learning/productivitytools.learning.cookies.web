b10a4e88596ebe3bcb69ebc53a7a4c706b325f21
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-07-03 21:52:53 +0200

    with credentials

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import { useState } from "react";
 import axios from "axios";
 
-
 function App() {
-
-  const [date,setDate]=useState()
-  const getDate= async ()=>{
-    var response=await axios.get("https://localhost:7278/Date/Get")
+  const [date, setDate] = useState();
+  const getDate = async () => {
+    var response = await axios.get("https://localhost:7278/Date/Get", { withCredentials: true });
     console.log(response);
-    setDate(response.data)
-  }
+    setDate(response.data);
+  };
 
   return (
     <div className="App">
-      <img src="G.png"/>
+      <img src="G.png" />
       <button onClick={getDate}>kliknij mnie</button>
       pawel
       <span>{date}</span>
-      <img src="https://localhost:7278/Date/Image"/>
+      <img src="https://localhost:7278/Date/Image" />
     </div>
   );
 }
